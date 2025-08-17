cd07e7746d9436ccab45e3040d08f6fd87d4540a
Vaibhav <sanapvaibhav8767@gmail.com>
2025-08-17 10:31:23 +0530

    fixed

--- server/models/user.js
+++ server/models/user.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose")
 
 const userSchema = new mongoose.Schema({
-  _id:{
+   clerkId:{
     type:String,
-     required:true
+     required:true,
+     unique: true
   },
   name:{
     type:String,
